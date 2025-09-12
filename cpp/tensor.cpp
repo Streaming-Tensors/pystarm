0fdb52b9c58770e1337e095588e1df5e099ab3fe
Aditya Devarakonda <1521781+aditya08@users.noreply.github.com>
2025-09-11 22:11:17 -0400

    fixed the issue where I implicitly assume each slice is a 3-way tensor instead of 2-way for tsvdm. introduced a new bool which can be used to swith code paths if dealing with jagged U vs. jagged sVt. updated comments

--- cpp/tensor.cpp
+++ cpp/tensor.cpp
@@ -122,48 +122,32 @@ class Tensor{
 };
 
 class JaggedTensor{
-    // This class currently supports jaggedness only in the last mode
-    // TODO: Support jaggedness in first mode (Vt is variable in row dim)
-    // TODO: Generalize to support jaggedness in any one mode (need to store mode index and update copy logic everywhere)
-    // TODO: Generalize to support jaggedness in multiple modes. Use case for this is not clear...
+    // This class currently supports jaggedness only in the second mode
+    // TODO: Generalize to support jaggedness in any one mode (need to store mode index and update copy logic in set/get)
+    // TODO: Generalize to support jaggedness in multiple modes? Use case for this and advantage over storing as a sparse tensor?
     public:
-    bool last_mode_jagged;  // right now, we implicitly assume this is set to true
-    size_t ndim;
+    size_t fixed_dim_size;
     size_t nslices;  // Number of slices
+    bool variable_first_mode;
     // Whether the last mode is jagged. If false, then first mode is jagged
-    std::vector<size_t> dims;  // Dimensions of the first ndim - 1 tensor
-    // !!! We assume that only the last dimension can vary
     std::vector<size_t> slice_ranks;  // Each element is the rank of the corresponding slice
     double *data_ptr;
 
-    // JaggedTensor() : ndim(0), nslices(0) {}
-
-    JaggedTensor(py::buffer &buf, size_t ndim, std::vector<size_t> dims, std::vector<size_t> slice_ranks)
-        : ndim(ndim), nslices(slice_ranks.size()) {
+    JaggedTensor(py::buffer &buf, size_t fixed_dim_size, std::vector<size_t> slice_ranks, bool variable_first_mode = false)
+        : fixed_dim_size(fixed_dim_size), nslices(slice_ranks.size()) {
         py::buffer_info buf_info = buf.request();
-        this->dims.resize(ndim);
         this->slice_ranks.resize(this->nslices);
-        for(size_t i = 0; i < ndim; i++){
-            this->dims[i] = dims[i];
-        }
         for (size_t i = 0; i < this->nslices; i++) {
             this->slice_ranks[i] = slice_ranks[i];
         }
         this->data_ptr = static_cast<double*>(buf_info.ptr);
     }
 
-    JaggedTensor(size_t buflen, size_t ndim, std::vector<size_t> dims, std::vector<size_t> slice_ranks)
-        : ndim(ndim), dims(dims), nslices(slice_ranks.size()), slice_ranks(slice_ranks) {
-        assert(ndim >= 2);
-
-        size_t expected_buflen = 1;
-        for (size_t dim_size: dims) {
-            expected_buflen *= dim_size;
-        }
-        size_t nrows = expected_buflen;
-        expected_buflen = 0;
+    JaggedTensor(size_t buflen, size_t fixed_dim_size, std::vector<size_t> slice_ranks)
+        : fixed_dim_size(fixed_dim_size), nslices(slice_ranks.size()), slice_ranks(slice_ranks) {
+        size_t expected_buflen = 0;
         for (size_t r : slice_ranks) {
-            expected_buflen += nrows * r;
+            expected_buflen += fixed_dim_size * r;
         }
         assert(buflen == expected_buflen);
         this->nslices = slice_ranks.size();
@@ -174,49 +158,31 @@ class JaggedTensor{
         this->data_ptr = static_cast<double*>(malloc(buflen*sizeof(double)));
     }
 
-    JaggedTensor(const JaggedTensor & obj) : ndim(obj.ndim), nslices(obj.nslices) {
-        this->dims.resize(ndim);
+    JaggedTensor(const JaggedTensor & obj) : fixed_dim_size(obj.fixed_dim_size), nslices(obj.nslices) {
         this->slice_ranks.resize(this->nslices);
-        for(size_t i = 0; i < ndim; i++){
-            this->dims[i] = obj.dims[i];
-        }
         for (size_t i = 0; i < this->nslices; i++) {
             this->slice_ranks[i] = obj.slice_ranks[i];
         }
-        size_t nrows = 1;
-        for (size_t i = 0; i < ndim - 1; i++) {
-            nrows = nrows * dims[i];
-        }
         size_t buflen = 0;
         for (size_t r : slice_ranks) {
-            buflen += nrows * r;
+            buflen += fixed_dim_size * r;
         }
         this->data_ptr = static_cast<double*>(malloc(buflen * sizeof(double)));
         std::copy(obj.data_ptr, obj.data_ptr + buflen, this->data_ptr);
     }
 
-    // Get tensor dimensions
-    std::tuple<std::vector<size_t>, std::vector<size_t>> getdims(){
-        // last dimension can vary, so return a tuple (dims, slice ranks)
-        return std::make_tuple(this->dims, this->slice_ranks);
-    }
-
     Matrix getfrontalslice(size_t i) {
         // Check if requesting a legal slice
         assert(i < this->nslices);
-        size_t nrows = 1;
-        for (size_t i = 0; i < this->ndim - 1; i++) {
-            nrows *= this->dims[i];
-        }
-        size_t ncols = this->slice_ranks[i];
-        size_t buflen = nrows * ncols;
+        size_t var_dim_size = this->slice_ranks[i];
+        size_t buflen = fixed_dim_size * var_dim_size;
 
         size_t start_idx = 0;
         for (size_t s = 0; s < i; s++) {
-            start_idx += nrows * this->slice_ranks[s];
+            start_idx += fixed_dim_size * this->slice_ranks[s];
         }
 
-        Matrix slice_mat(buflen, nrows, ncols);
+        Matrix slice_mat(buflen, fixed_dim_size, var_dim_size);
         std::copy(this->data_ptr + start_idx,
             this->data_ptr + (start_idx + buflen), slice_mat.data_ptr);
 
@@ -226,19 +192,20 @@ class JaggedTensor{
     void setfrontalslice(const Matrix &slice, size_t i) {
         // Check if requesting a legal slice
         assert(i < this->nslices);
-        size_t nrows = 1;
-        for (size_t i = 0; i < this->ndim - 1; i++) {
-            nrows *= this->dims[i];
+        size_t var_dim_size = this->slice_ranks[i];
+        size_t buflen = fixed_dim_size * var_dim_size;
+        if (variable_first_mode) {
+            assert(slice.nrow == var_dim_size);
+            assert(slice.ncol == fixed_dim_size);
         }
-        size_t ncols = this->slice_ranks[i];
-        size_t buflen = nrows * ncols;
-        // TODO: permute if first mode is jagged (i.e. this is a Vt slice)
-        assert(slice.nrow == nrows);
-        assert(slice.ncol == ncols);
+        else{
+            assert(slice.nrow == fixed_dim_size);
+            assert(slice.ncol == var_dim_size);
+        }
 
         size_t start_idx = 0;
         for (size_t s = 0; s < i; s++) {
-            start_idx += nrows * this->slice_ranks[s];
+            start_idx += fixed_dim_size * this->slice_ranks[s];
         }
 
         // Copy to the slice
@@ -250,7 +217,6 @@ class JaggedTensor{
         if (this->data_ptr != nullptr){
             free(this->data_ptr);
         }
-        this->dims.clear();
         this->slice_ranks.clear();
     }
 };
