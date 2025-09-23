6e70c1d73b7a469d85dab28ce987bf3bd34215bc
Srinivas Eswar <seswar@anl.gov>
2025-09-23 11:21:29 -0500

    Keeping memory prints off as default.

--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -11,9 +11,9 @@
 #include <omp.h>
 #include <mkl.h>
 
-// Debug printing
-#ifndef _MEMPRINT
-#define _MEMPRINT 1
-#endif
+// Debug printing (uncomment for prints)
+// #ifndef _MEMPRINT
+// #define _MEMPRINT 1
+// #endif
 
 #endif
